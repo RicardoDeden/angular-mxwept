7d7ebe8aa5cc84f3b1ff34fd53119a5809d4a4e7
ricardodeden <r.d.deden@hotmail.nl>
2022-10-06 14:25:01 +0200

    Added checkout form and submit

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from "../cart.service";
+import {Observable} from "rxjs";
+
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShippingComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private cartService: CartService
+  ) { }
+
+  shippingCosts! : Observable<{ type: string, price: number }[]>
 
   ngOnInit(): void {
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
 
 }
